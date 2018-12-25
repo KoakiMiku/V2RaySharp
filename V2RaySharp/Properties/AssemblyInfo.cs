5e166663664a7fab5d9f3b4572b739006bf88a4f
KoakiMiku <2234975857@qq.com>
2018-12-25 22:11:29 +0800

    modify config

--- V2RaySharp/Properties/AssemblyInfo.cs
+++ V2RaySharp/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: AssemblyTitle("V2RaySharp")]
 [assembly: AssemblyDescription("V2RaySharp")]
 [assembly: AssemblyProduct("V2RaySharp")]
-[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
 [assembly: AssemblyCopyright("Copyright © 2018 KoakiMiku")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
