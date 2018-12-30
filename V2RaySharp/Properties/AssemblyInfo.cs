3de5f86252132dd8f2e829f96a57abedebc9075b
KoakiMiku <2234975857@qq.com>
2018-12-30 17:02:06 +0800

    change copyright

--- V2RaySharp/Properties/AssemblyInfo.cs
+++ V2RaySharp/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Windows;
 [assembly: AssemblyDescription("V2RaySharp")]
 [assembly: AssemblyProduct("V2RaySharp")]
 [assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyCopyright("Copyright © 2018 KoakiMiku")]
+[assembly: AssemblyCopyright("Copyright © KoakiMiku 2019")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
