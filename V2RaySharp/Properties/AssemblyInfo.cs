84e6702b6f95e2c8e913a18a102f09db7d8859c0
KoakiMiku <2234975857@qq.com>
2019-04-25 00:03:11 +0800

    change systemproxy to sysproxy

--- V2RaySharp/Properties/AssemblyInfo.cs
+++ V2RaySharp/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 [assembly: AssemblyTitle("V2RaySharp")]
 [assembly: AssemblyDescription("V2RaySharp")]
 [assembly: AssemblyProduct("V2RaySharp")]
-[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
 [assembly: AssemblyCopyright("Copyright © KoakiMiku 2019")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
