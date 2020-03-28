796376335abde29188434a5c8ae12681049e2f23
KoakiMiku <2234975857@qq.com>
2020-03-28 22:46:46 +0800

    upgrade to netcoreapp3.1

--- V2RaySharp/Properties/Resources.Designer.cs
+++ V2RaySharp/Properties/Resources.Designer.cs
@@ -111,12 +111,12 @@ namespace V2RaySharp.Properties {
         }
         
         /// <summary>
-        ///   查找类似于 (图标) 的 System.Drawing.Icon 类型的本地化资源。
+        ///   查找 System.Byte[] 类型的本地化资源。
         /// </summary>
-        internal static System.Drawing.Icon V2Ray {
+        internal static byte[] V2Ray {
             get {
                 object obj = ResourceManager.GetObject("V2Ray", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
+                return ((byte[])(obj));
             }
         }
     }
